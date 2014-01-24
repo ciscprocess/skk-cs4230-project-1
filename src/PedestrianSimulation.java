cc0922a0195c74938fa68ae9d5565eab107e20fc
Nathan Korzekwa <ciscprocess@gmail.com>
2014-01-24 17:10:32 -0500

    Initial template for project. I will work on the other GUI elements later.

---
+++ src/PedestrianSimulation.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class PedestrianSimulation {
+
+	public static void main(String[] args) {
+		JFrame meFrame = new JFrame("Pedestrian Simulation");
+		meFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//meFrame.add(panel);
+		meFrame.pack();
+		meFrame.setVisible(true);
+		
+	}
+
+}
