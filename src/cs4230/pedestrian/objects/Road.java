fd3ba2d17f7c3ca6daa40d4fd4b8eda4777306c4
Nathan Korzekwa <ciscprocess@gmail.com>
2014-02-01 21:34:19 -0500

    Added Road class

---
+++ src/cs4230/pedestrian/objects/Road.java
@@ -0,0 +1,10 @@
+package cs4230.pedestrian.objects;
+
+public class Road extends Cell {
+
+	public Road(int x, int y, double mult) {
+		super(x, y, mult);
+		// TODO Auto-generated constructor stub
+	}
+
+}
